310bff271eba24d7b5c5d851c06c4c2a592de280
Mikkel Birkedam <mikkelsteen@gmail.com>
2022-03-10 19:51:45 +0100

    remove log

--- archive.go
+++ archive.go
@@ -9,17 +9,6 @@ import (
 	"github.com/mikkelstb/feedfetcher/feed"
 )
 
-func init() {
-
-	f, err := os.OpenFile("log.log", os.O_RDWR|os.O_CREATE|os.O_APPEND, 0666)
-	if err != nil {
-		panic(err.Error())
-	}
-	defer f.Close()
-
-	log.SetOutput(f)
-}
-
 type Archive struct {
 	path string
 }
@@ -44,7 +33,6 @@ func NewArchive(path string) (*Archive, error) {
 
 func (a Archive) writeNewsItemAsJson(ni feed.NewsItem) (string, error) {
 	filename := fmt.Sprintf("%03d_%v_%v.json", ni.FeedId, ni.Docdate.Format("0601021504"), ni.Id())
-	//filename := strings.Join([]string{ni.FeedId, ni.Docdate.Format("_0601021504_"), ni.GetId(), ".json"}, "")
 	folder_path := filepath.Join(a.path, ni.Feed, ni.Docdate.Format("2006/01"))
 
 	// Check if folder exists
@@ -79,40 +67,3 @@ func (a Archive) checkFolder(path string) error {
 	}
 	return nil
 }
-
-// func (a Archive) getSources() ([]string, error) {
-// 	list, err := os.ReadDir(a.path)
-// 	if err != nil {
-// 		return nil, err
-// 	}
-// 	var sources []string
-// 	for i := range list {
-// 		if list[i].IsDir() {
-// 			sources = append(sources, list[i].Name())
-// 		}
-// 	}
-// 	return sources, nil
-// }
-
-// func (a Archive) writeNewsItem(ni feed.NewsItem) (string, error) {
-// 	filename := strings.Join([]string{ni.Feed, ni.Docdate.Format("_20060102_"), ni.GetId(), ".xml"}, "")
-// 	folder_path := filepath.Join(a.path, ni.Feed, ni.Docdate.Format("2006/01/02"))
-
-// 	// Check if folder exists
-// 	// If not: try create
-// 	a.checkFolder(folder_path)
-
-// 	//Convert NewsItem to NewsItemXML
-// 	xml_data, _ := ni.ToXml()
-
-// 	if _, err := os.Stat(filepath.Join(folder_path, filename)); err == nil {
-// 		return "", fmt.Errorf("skipping file %v, file already exists", filename)
-// 	}
-
-// 	err := os.WriteFile(filepath.Join(folder_path, filename), xml_data, 0666)
-// 	if err != nil {
-// 		return "", err
-// 	}
-
-// 	return filename, nil
-// }
